4eac237a798cb9558b0ceb4dc5feb8f3c90863fb
Chris Held <cheld100@gmail.com>
2014-07-31 13:02:08 -0500

    switch to async.forever

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,32 +6,42 @@ var async = require('async'),
 module.exports = function(grunt) {
   // Do grunt-related things in here
   grunt.registerTask('default', 'Check some servers', function(){
-    var me = this;
+    //this is telling grunt that this is an async task, not to be confused with
+    //the async module
+    var done = this.async();
 
-    var done = me.async();
-    setInterval(function(){
-      async.each(config.servers, function(server, cb){
-        var start = new Date().getTime();
-        var req = http.request(server, function(res) { 
-          var end = new Date().getTime();
-          var took = end - start;
-          grunt.log.writeln("Got " + server.host + " on port " + server.port + " took " + took + "ms");
-          if(took > 500) {
-            //TODO took too long(make ttl configurable), send a warning email
-          }
-          cb(); 
-        });
-        req.on('error', function(err) {
-          cb(err);
-        });
-        req.end();
-      }, function(err){
-        if(err) {
-          grunt.log.writeln('problem with get: ' + err.message);
-          //TODO - send email
-        }
-        //done();
-      });
-    }, config.waitInterval * 1000);
+    async.forever(
+      function(next){
+        setInterval(function(){
+          async.each(config.servers, function(server, cb){
+            var start = new Date().getTime();
+            var req = http.request(server, function(res) { 
+              var end = new Date().getTime();
+              var took = end - start;
+              grunt.log.writeln("Got " + server.host + " on port " + server.port + " took " + took + "ms");
+              if(took > 500) {
+                //TODO took too long(make ttl configurable), send a warning email
+              }
+              cb(); 
+            });
+            req.on('error', function(err) {
+              cb(err);
+            });
+            req.end();
+          }, function(err){
+            if(err) {
+              grunt.log.writeln('problem with get: ' + err.message);
+              next(err);
+              //TODO - send email
+            }
+            next();
+          });
+        }, config.waitInterval * 1000);
+      },
+      function(err){
+        done();
+      }
+    );
+
   });
 };
